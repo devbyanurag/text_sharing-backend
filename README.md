d7d141dd48cf8614f4215eacdd86af63d059807c
Anurag Chhetri <anurag.chhetri@nagarro.com>
2024-12-24 13:06:28 +0530

    text_sharing-backend

---
+++ README.md
@@ -0,0 +1,25 @@
+# CastView
+CastView is a platform designed for managing casting requirements for various projects. Users can add their casting requirements and view all posted requirements.
+
+
+## Features
+
+1: Add Casting Requirements: Users can submit details of casting requirements for their projects.
+2: View Requirements: Browse and filter through all casting requirements posted on the platform.
+
+## Technologies Used
+
+* Frontend: React.js, TypeScript
+* Backend: Node.js, Express
+* Database: MongoDB
+* Authentication: JWT (JSON Web Tokens)
+
+Create .env with these values:
+
+MONGO_URL,JWT_SECRET,FRONTEND_URL,IMAGEKIT_PRIVATE,IMAGEKIT_PUBLIC,PORT
+
+## frontend github
+ [frontend github click here](https://github.com/devbyanurag/CastView-frontend)
+
+
+
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+
+
+// const postRoutes = require("./src/routes/Post");
+// const userRoute = require("./src/routes/User");
+
+
+const allowedOrigins = [
+    process.env.FRONTEND_URL, 
+    "*"
+];
+
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("API Working on port 5000 ");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
